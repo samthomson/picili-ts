21dba7004289d67e02a12d62c6864110f2e8b82e
Sam <samt@samt.st>
2021-10-18 19:26:51 +0200

    server with both gql and http support

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,1 +1,43 @@
-console.log('server running???')
+import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import express from 'express'
+import http from 'http'
+
+const typeDefs = gql`
+	type Query {
+		ping: String
+	}
+`
+
+const resolvers = {
+    Query: {
+		ping: () => 'pinged'
+	},
+}
+
+
+
+const startApolloServer = async (typeDefs, resolvers) => {
+	const app = express()
+
+	app.get('/oauth/dropbox', (req, res) => {
+		res.send('response')
+	})
+
+	const httpServer = http.createServer(app)
+	const server = new ApolloServer({
+	  typeDefs,
+	  resolvers,
+	  plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageGraphQLPlayground()],
+	})
+
+	await server.start()
+	
+	server.applyMiddleware({ app })
+
+	httpServer.listen({ port: 4000 }, () => {
+		console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+	})
+}
+
+startApolloServer(typeDefs, resolvers)
