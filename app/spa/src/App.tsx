4fcd125bc86b3cbbc79171e4ef05e233c76a0a5d
Sam <samt@samt.st>
2021-10-19 13:31:58 +0200

    client linted

--- app/spa/src/App.tsx
+++ app/spa/src/App.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 const App = () => {
+	const search = useLocation().search
+	const token = new URLSearchParams(search).get('code')
 
-  const search = useLocation().search;
-  const token = new URLSearchParams(search).get('code');
+	const dropboxOAuth = () => {
+		window.location.replace(`http://localhost:3501/oauth/dropbox`)
+	}
 
-  const dropboxOAuth = () => {    
-    window.location.replace(`http://localhost:3501/oauth/dropbox`)
-  }
-
-  return (
-    <div>
-      app
-      {token || 'no token'}
-      <button onClick={dropboxOAuth}>dropbox oauth</button>
-    </div>
-  );
+	return (
+		<div>
+			app
+			{token || 'no token'}
+			<button onClick={dropboxOAuth}>dropbox oauth</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- app/spa/src/components/dropboxConnection.tsx
+++ app/spa/src/components/dropboxConnection.tsx
@@ -2,28 +2,27 @@ import React from 'react'
 import { Redirect, useLocation } from 'react-router-dom'
 
 const DropboxConnection = () => {
+	const search = useLocation().search
+	const token = new URLSearchParams(search).get('code')
 
-  const search = useLocation().search;
-  const token = new URLSearchParams(search).get('code');
+	const dropboxOAuth = () => {
+		window.location.replace(`http://localhost:3501/oauth/dropbox`)
+	}
 
-  const dropboxOAuth = () => {    
-    window.location.replace(`http://localhost:3501/oauth/dropbox`)
-  }
+	// if (!token) {
+	//   return <>
+	//     <p>redirecting to Dropbox OAuth</p>
+	//     <Redirect to="http://localhost:3501/oauth/dropbox" />
+	//   </>
+	// }
 
-  // if (!token) {
-  //   return <>
-  //     <p>redirecting to Dropbox OAuth</p>
-  //     <Redirect to="http://localhost:3501/oauth/dropbox" />
-  //   </>
-  // }
-
-  return (
-    <div>
-      app
-      {token || 'no token'}
-      <button onClick={dropboxOAuth}>dropbox oauth</button>
-    </div>
-  );
+	return (
+		<div>
+			app
+			{token || 'no token'}
+			<button onClick={dropboxOAuth}>dropbox oauth</button>
+		</div>
+	)
 }
 
 export default DropboxConnection
