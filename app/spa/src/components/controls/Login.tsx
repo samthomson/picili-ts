2de8acd63405df9fb02a0b81d94e3fd3f970cf9e
Sam <samt@samt.st>
2021-10-20 12:17:36 +0200

    spa working login form

--- app/spa/src/components/controls/Login.tsx
+++ app/spa/src/components/controls/Login.tsx
@@ -1,55 +1,54 @@
 import * as React from 'react'
 import * as ReactRedux from 'react-redux'
-// import { useMutation, gql } from '@apollo/client'
-// import classNames from 'classnames'
+import { useMutation, gql } from '@apollo/client'
+import classNames from 'classnames'
 import { NavLink } from 'react-router-dom'
 
 import * as Actions from 'src/redux/actions'
 
-// const loginQuery = gql`
-// 	mutation login($authInput: LoginInput!) {
-// 		login(authInput: $authInput) {
-// 			token
-// 			error
-// 		}
-// 	}
-// `
+const loginQuery = gql`
+	mutation login($authInput: LoginInput!) {
+		login(authInput: $authInput) {
+			token
+			error
+		}
+	}
+`
 
 const Login: React.FunctionComponent = () => {
-	// const [email, setEmail] = React.useState<string>('')
-	// const [password, setPassword] = React.useState<string>('')
-	// const [error, setError] = React.useState<string>('')
+	const [email, setEmail] = React.useState<string>('')
+	const [password, setPassword] = React.useState<string>('')
+	const [error, setError] = React.useState<string>('')
 
-	// const [loginMutation, { error: httpError, data, loading = false }] =
-	// 	useMutation(loginQuery)
+	const [loginMutation, { error: httpError, data, loading = false }] =
+		useMutation(loginQuery)
 
-	// const dispatch = ReactRedux.useDispatch()
+	const dispatch = ReactRedux.useDispatch()
 
-	// React.useEffect(() => {
-	// 	if (data?.login.token) {
-	// 		dispatch(Actions.attemptLoginSucceeded(data.login.token))
-	// 	}
-	// 	if (data?.login.error) {
-	// 		setError(data.login.error)
-	// 	}
-	// }, [data])
+	React.useEffect(() => {
+		if (data?.login.token) {
+			dispatch(Actions.attemptLoginSucceeded(data.login.token))
+		}
+		if (data?.login.error) {
+			setError(data.login.error)
+		}
+	}, [data])
 
-	// React.useEffect(() => {
-	// 	dispatch(Actions.setGlobalLoadingState(loading))
-	// }, [loading])
+	React.useEffect(() => {
+		dispatch(Actions.setGlobalLoadingState(loading))
+	}, [loading])
 
-	// const loginHandler = async (e: React.FormEvent) => {
-	// 	e.preventDefault()
-	// 	await loginMutation({ variables: { authInput: { email, password } } })
-	// }
+	const loginHandler = async (e: React.FormEvent) => {
+		e.preventDefault()
+		await loginMutation({ variables: { authInput: { email, password } } })
+	}
 
-	// const loginFailed = httpError?.message || data?.login.error || error
-	// const formDisabled = loading || !(email !== '' && password !== '')
+	const loginFailed = httpError?.message || data?.login.error || error
+	const formDisabled = loading || !(email !== '' && password !== '')
 
 	return (
 		<React.Fragment>
-			[login page]
-			{/* <div className="ui middle aligned center aligned grid">
+			<div className="ui middle aligned center aligned grid">
 				<div className="middle-form-column">
 					<h2 className="ui header">
 						<div className="content">Login</div>
@@ -131,7 +130,7 @@ const Login: React.FunctionComponent = () => {
 						</NavLink>
 					</div>
 				</div>
-			</div> */}
+			</div>
 		</React.Fragment>
 	)
 }
