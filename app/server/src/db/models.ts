0c8e9a9566cf00a817d02f3b228f8b53923469ce
Sam <samt@samt.st>
2021-11-06 20:31:49 +0000

    dropbox connection orm model

--- app/server/src/db/models.ts
+++ app/server/src/db/models.ts
@@ -29,3 +29,38 @@ export const UserModel = Database.define<UserInstance>(
         underscored: true,
     },
 )
+
+interface DropboxConnectionAttributes {
+    id: string
+    userId: number
+    token: string
+    syncPath?: string
+    syncEnabled?: boolean
+}
+type DropboxConnectionCreationAttributes = Sequelize.Optional<DropboxConnectionAttributes, 'id'>
+
+export interface DropboxConnectionInstance
+    extends Sequelize.Model<DropboxConnectionAttributes, DropboxConnectionCreationAttributes>,
+        UserAttributes {
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export const DropboxConnectionModel = Database.define<DropboxConnectionInstance>(
+    'dropbox_connection',
+    {
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        userId: Sequelize.INTEGER.UNSIGNED,
+        token: Sequelize.STRING,
+        syncPath: Sequelize.STRING,
+        syncEnabled: Sequelize.BOOLEAN,
+    },
+    {
+        timestamps: true,
+        underscored: true,
+    },
+)
