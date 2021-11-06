134e30671d3421ec62ef84cc251f7ddeb298e73e
Sam <samt@samt.st>
2021-11-06 19:28:17 +0000

    dropbox connection migration

---
+++ app/server/src/db/migrations/002_dropbox_connection_create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `dropbox_connection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `sync_path` varchar(255) DEFAULT NULL,
+  `sync_enabled` TINYINT(1) DEFAULT 0,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3;
