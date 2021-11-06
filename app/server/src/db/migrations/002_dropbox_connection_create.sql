fd85767dbda1b07c3ef6726468a526ed37e1408f
Sam <samt@samt.st>
2021-11-06 20:31:21 +0000

    update dropbox connection migration

--- app/server/src/db/migrations/002_dropbox_connection_create.sql
+++ app/server/src/db/migrations/002_dropbox_connection_create.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS `dropbox_connection` (
+CREATE TABLE IF NOT EXISTS `dropbox_connections` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
+  `token` varchar(255),
   `sync_path` varchar(255) DEFAULT NULL,
   `sync_enabled` TINYINT(1) DEFAULT 0,
   `created_at` datetime NOT NULL,
