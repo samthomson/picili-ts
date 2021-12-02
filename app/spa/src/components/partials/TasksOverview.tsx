35fd86f9cf459cd82f6c0912ef767d2a00738690
Sam <samt@samt.st>
2021-12-02 17:35:49 +0000

    display recently processed tasks

--- app/spa/src/components/partials/TasksOverview.tsx
+++ app/spa/src/components/partials/TasksOverview.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useQuery, gql } from '@apollo/client'
 import moment from 'moment'
 
+import * as Types from '@shared/declarations'
+
 const taskSummaryQuery = gql`
 	query taskSummary {
 		taskSummary {
@@ -35,10 +37,12 @@ const TasksOverview: React.FunctionComponent = () => {
 		return <>{error?.message}</>
 	}
 
+	const taskSummary: Types.API.TaskSummary = data.taskSummary
 	const {
 		oldest,
-		processable: { total, actionable },
-	} = data.taskSummary
+		processable: { total, actionable, queues },
+		processed: { recent },
+	} = taskSummary
 
 	const queueAge = !oldest
 		? 'no tasks, or tasks are all scheduled in the future'
@@ -55,6 +59,41 @@ const TasksOverview: React.FunctionComponent = () => {
 			actionable tasks: {actionableTasks}
 			<br />
 			<h2>queues</h2>
+			{queues.length === 0 && <>no tasks queued...</>}
+			{queues.length > 0 && (
+				<table>
+					<tr>
+						<td>type</td>
+						<td>count</td>
+					</tr>
+					{queues.map((row, i) => (
+						<tr key={i}>
+							<td>
+								{row.type.replaceAll('_', ' ').toLowerCase()}
+							</td>
+							<td>{row.count}</td>
+						</tr>
+					))}
+				</table>
+			)}
+			<h2>processed</h2>
+			{recent.length === 0 && (
+				<>no tasks processed in the last month...</>
+			)}
+			{recent.length > 0 && (
+				<table>
+					<tr>
+						<td>date</td>
+						<td>count</td>
+					</tr>
+					{recent.map((row, i) => (
+						<tr key={i}>
+							<td>{moment(row.date).format('MMM Do')}</td>
+							<td>{row.count}</td>
+						</tr>
+					))}
+				</table>
+			)}
 		</React.Fragment>
 	)
 }
