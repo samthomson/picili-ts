691e539db80dad7906ba26a786a55c43e0f0f727
Sam <samt@samt.st>
2022-01-14 20:36:33 +0000

    updated comment - multi command works with semi

--- initial-deployment.sh
+++ initial-deployment.sh
@@ -3,7 +3,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/.env
 
-# next line doesn't work for some reason? doing it manually does
 docker-machine ssh $DOCKER_MACHINE_NAME "cd /; git clone https://github.com/samthomson/picili-ts.git"
 
 docker-machine scp .env $DOCKER_MACHINE_NAME:/picili-ts/.env
