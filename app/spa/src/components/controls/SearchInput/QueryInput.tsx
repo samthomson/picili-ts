63578dcba2bf263c63cfba32843a8d1d0fa67fde
Sam <samt@samt.st>
2022-01-19 16:30:26 +0000

    add query on pressing return

--- app/spa/src/components/controls/SearchInput/QueryInput.tsx
+++ app/spa/src/components/controls/SearchInput/QueryInput.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react'
+import * as ReactRedux from 'react-redux'
+
+import * as Actions from 'src/redux/actions'
 
 import TypeAhead from './TypeAhead'
 
 const QueryInput: React.FunctionComponent = () => {
+	const dispatch = ReactRedux.useDispatch()
+
+	const [textInputValue, setTextInputValue] = React.useState<string>('')
+
+	// return pressed: parse/add as query to redux and clear textInputValue
+	const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === 'Enter') {
+			// parse/add to query
+			const add = {
+				type: '',
+				subtype: '',
+				value: textInputValue,
+			}
+			dispatch(Actions.searchQueryAdd(add))
+			// clear input
+			setTextInputValue('')
+			// do search
+		}
+	}
+
 	return (
 		<div id="query-input">
-			<input type="text" placeholder="add query..." />
-			<TypeAhead />
+			<input
+				type="text"
+				placeholder="add query..."
+				value={textInputValue}
+				onChange={(e) => setTextInputValue(e.target.value)}
+				onKeyDown={onKeyDown}
+			/>
+			<TypeAhead currentTextInputValue={textInputValue} />
 		</div>
 	)
 }
