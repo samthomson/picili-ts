71508acdf0d707b5ba9c5588e86c695562da852f
Sam <samt@samt.st>
2022-01-19 16:30:19 +0000

    type ahead functional skeleton / todos

--- app/spa/src/components/controls/SearchInput/TypeAhead.tsx
+++ app/spa/src/components/controls/SearchInput/TypeAhead.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react'
 
-const TypeAhead: React.FunctionComponent = () => {
-	return <div id="type-ahead">[type-ahead]</div>
+interface IProps {
+	currentTextInputValue: string
+}
+
+const TypeAhead: React.FunctionComponent<IProps> = ({
+	currentTextInputValue,
+}) => {
+	// todo: effect watching for `currentTextInputValue` changing, if something call typeahead service
+	// todo: effect watching `currentTextInputValue` changing from something to nothing, cancel any request
+	// todo: likewise cancel on unmount
+
+	if (!currentTextInputValue) {
+		return <></>
+	}
+	return (
+		<div id="type-ahead">[type-ahead ui for {currentTextInputValue}]</div>
+	)
 }
 
 export default TypeAhead
