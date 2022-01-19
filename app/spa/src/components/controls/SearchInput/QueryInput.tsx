eb56296fc00bc1c564882775d4dfa7202bd401b4
Sam <samt@samt.st>
2022-01-19 17:23:43 +0000

    parse text input into a query

--- app/spa/src/components/controls/SearchInput/QueryInput.tsx
+++ app/spa/src/components/controls/SearchInput/QueryInput.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactRedux from 'react-redux'
 
+import * as Types from '@shared/declarations'
 import * as Actions from 'src/redux/actions'
 
 import TypeAhead from './TypeAhead'
@@ -10,15 +11,47 @@ const QueryInput: React.FunctionComponent = () => {
 
 	const [textInputValue, setTextInputValue] = React.useState<string>('')
 
+	const parseTextToQuery = (
+		text: string,
+	): Types.API.IndividualSearchQuery => {
+		// split on equals sign
+		let type = undefined
+		let subtype = undefined
+		let value = ''
+		const typeValueParts = text.split('=')
+		if (typeValueParts.length === 1) {
+			// take the only part as the value
+			value = typeValueParts[0]
+		} else {
+			// at least two parts
+			const [typePart, ...valueParts] = typeValueParts
+			// split first part into type/subtype
+			const typeSubtypeParts = typePart.split('.')
+			// set the value via a join (incase there were multiple chunks)
+			value = valueParts.join('=')
+
+			if (typeSubtypeParts.length === 1) {
+				// no subtype, just a type
+				type = typeSubtypeParts[0]
+			} else {
+				const [parsedType, ...subtypes] = typeSubtypeParts
+				type = parsedType
+				subtype = subtypes.join('.')
+			}
+		}
+
+		return {
+			type,
+			subtype,
+			value,
+		}
+	}
+
 	// return pressed: parse/add as query to redux and clear textInputValue
 	const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
 		if (event.key === 'Enter') {
 			// parse/add to query
-			const add = {
-				type: '',
-				subtype: '',
-				value: textInputValue,
-			}
+			const add = parseTextToQuery(textInputValue)
 			dispatch(Actions.searchQueryAdd(add))
 			// clear input
 			setTextInputValue('')
