78526cf93e5c9c169fee43bb907b5a575fbab2df
Sam <samt@samt.st>
2022-01-19 15:57:52 +0000

    remove query

--- app/spa/src/components/controls/SearchInput/IndividualQuery.tsx
+++ app/spa/src/components/controls/SearchInput/IndividualQuery.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
+import * as ReactRedux from 'react-redux'
+
 import * as Types from '@shared/declarations'
+import * as Actions from 'src/redux/actions'
 interface IProps {
 	individualQuery: Types.API.IndividualSearchQuery
 }
@@ -7,7 +10,38 @@ interface IProps {
 const IndividualQuery: React.FunctionComponent<IProps> = ({
 	individualQuery,
 }) => {
-	return <div className="individual-query">[{individualQuery.value}]</div>
+	const dispatch = ReactRedux.useDispatch()
+
+	const removeQuery = (
+		type: string | undefined = undefined,
+		subtype: string | undefined = undefined,
+		value: string,
+	) => {
+		dispatch(
+			Actions.searchQueryRemove({
+				type,
+				subtype,
+				value,
+			}),
+		)
+	}
+
+	return (
+		<div className="individual-query">
+			{individualQuery.value}
+			<button
+				onClick={() =>
+					removeQuery(
+						individualQuery?.type ?? undefined,
+						individualQuery?.subtype ?? undefined,
+						individualQuery.value,
+					)
+				}
+			>
+				remove
+			</button>
+		</div>
+	)
 }
 
 export default IndividualQuery
