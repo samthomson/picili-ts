24dc382e93001e26712536b5d8ccfbc885cd1277
Sam <samt@samt.st>
2022-01-20 19:25:31 +0000

    refactor search sagas to share code

--- app/spa/src/redux/sagas.ts
+++ app/spa/src/redux/sagas.ts
@@ -22,56 +22,63 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 })
 
+// const callSearchQuery = async (): Promise<Types.API.SearchResult> => {
+function* callSearchQuery(page = 1) {
+	const searchFilter: Types.API.SearchQuery = yield select(
+		Selectors.searchQuery,
+	)
+	const response: { data: { search: Types.API.SearchResult } } =
+		yield client.mutate({
+			mutation: gql`
+				query (
+					$searchFilter: SearchFilter!
+					$page: Int
+					$perPage: Int
+				) {
+					search(
+						filter: $searchFilter
+						page: $page
+						perPage: $perPage
+					) {
+						pageInfo {
+							totalPages
+							totalItems
+							page
+							perPage
+							hasNextPage
+							hasPreviousPage
+						}
+						items {
+							uuid
+							userId
+							mediumWidth
+							mediumHeight
+							address
+							latitude
+						}
+						stats {
+							speed
+						}
+					}
+				}
+			`,
+			variables: {
+				searchFilter,
+				page,
+				// todo: remove (default 100 is fine)
+				perPage: 10,
+			},
+		})
+
+	const { items, pageInfo, stats } = response.data.search
+	return { items, pageInfo, stats }
+}
+
 function* search() {
 	yield put(Actions.searchingSet(true))
 	try {
-		const searchFilter: Types.API.SearchQuery = yield select(
-			Selectors.searchQuery,
-		)
-		const response: { data: { search: Types.API.SearchResult } } =
-			yield client.mutate({
-				mutation: gql`
-					query (
-						$searchFilter: SearchFilter!
-						$page: Int
-						$perPage: Int
-					) {
-						search(
-							filter: $searchFilter
-							page: $page
-							perPage: $perPage
-						) {
-							pageInfo {
-								totalPages
-								totalItems
-								page
-								perPage
-								hasNextPage
-								hasPreviousPage
-							}
-							items {
-								uuid
-								userId
-								mediumWidth
-								mediumHeight
-								address
-								latitude
-							}
-							stats {
-								speed
-							}
-						}
-					}
-				`,
-				variables: {
-					searchFilter,
-					page: 1,
-					// todo: remove (default 100 is fine)
-					perPage: 10,
-				},
-			})
-
-		const searchResult = response?.data?.search
+		// const searchResult = response?.data?.search
+		const searchResult: Types.API.SearchResult = yield callSearchQuery()
 		if (searchResult) {
 			yield put(Actions.attemptSearchSucceeded(searchResult))
 		} else {
@@ -89,57 +96,13 @@ function* search() {
 function* searchNext() {
 	yield put(Actions.searchingSet(true))
 	try {
-		const searchFilter: Types.API.SearchQuery = yield select(
-			Selectors.searchQuery,
-		)
 		const paginationInfo: Types.API.PaginationInfo = yield select(
 			Selectors.searchPaginationInfo,
 		)
 		const nextPage = paginationInfo.page + 1
-		const response: { data: { search: Types.API.SearchResult } } =
-			yield client.mutate({
-				mutation: gql`
-					query (
-						$searchFilter: SearchFilter!
-						$page: Int
-						$perPage: Int
-					) {
-						search(
-							filter: $searchFilter
-							page: $page
-							perPage: $perPage
-						) {
-							pageInfo {
-								totalPages
-								totalItems
-								page
-								perPage
-								hasNextPage
-								hasPreviousPage
-							}
-							items {
-								uuid
-								userId
-								mediumWidth
-								mediumHeight
-								address
-								latitude
-							}
-							stats {
-								speed
-							}
-						}
-					}
-				`,
-				variables: {
-					searchFilter,
-					page: nextPage,
-					// todo: remove (default 100 is fine)
-					perPage: 10,
-				},
-			})
-
-		const searchResult = response?.data?.search
+		const searchResult: Types.API.SearchResult = yield callSearchQuery(
+			nextPage,
+		)
 		if (searchResult) {
 			yield put(Actions.nextSearchSucceeded(searchResult))
 		} else {
@@ -156,10 +119,13 @@ function* searchNext() {
 
 function* watchSearch() {
 	yield takeLatest(Actions.ActionType.SEARCH_ATTEMPT, search)
+}
+
+function* watchSearchNext() {
 	yield takeLatest(Actions.ActionType.SEARCH_NEXT, searchNext)
 }
 
 // eslint-disable-next-line
 export default function* rootSaga() {
-	yield all([watchSearch()])
+	yield all([watchSearch(), watchSearchNext()])
 }
