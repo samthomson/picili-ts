ab4d1637e734c6a901bb62a67559c201a0dda92f
Sam <samt@samt.st>
2022-01-22 16:35:00 +0000

    rough lightbox logic

--- app/spa/src/components/controls/Lightbox/index.tsx
+++ app/spa/src/components/controls/Lightbox/index.tsx
@@ -1,18 +1,38 @@
 import * as React from 'react'
+import * as ReactRedux from 'react-redux'
+import classNames from 'classnames'
+
+import * as Actions from 'src/redux/actions'
+import * as Selectors from 'src/redux/selectors'
 
 import LightboxInfo from './LightboxInfo'
 
 const Lightbox: React.FunctionComponent = () => {
-	// get lightboxIndex from redux
+	const dispatch = ReactRedux.useDispatch()
+
+	const lightboxIndex = ReactRedux.useSelector(Selectors.lightboxIndex)
 
 	const [isInfoShowing, setIsInfoShowing] = React.useState<boolean>(false)
 
+	const close = () => dispatch(Actions.lightboxClose())
+	const previous = () => dispatch(Actions.lightboxPrevious())
+	const next = () => dispatch(Actions.lightboxNext())
+
 	return (
-		<div id="lightbox">
+		<div
+			id="lightbox"
+			className={classNames({
+				open: !!lightboxIndex,
+			})}
+		>
+			[{lightboxIndex}]
 			<LightboxInfo isShowing={isInfoShowing} />
 			<button onClick={() => setIsInfoShowing(!isInfoShowing)}>
 				show/hide info
 			</button>
+			<button onClick={close}>close</button>
+			<button onClick={previous}>left</button>
+			<button onClick={next}>right</button>
 		</div>
 	)
 }
--- app/spa/src/components/controls/SearchResults/TiledImageGallery.tsx
+++ app/spa/src/components/controls/SearchResults/TiledImageGallery.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
+import * as ReactRedux from 'react-redux'
 
 import * as Types from '@shared/declarations'
+import * as Actions from 'src/redux/actions'
 import * as HelperUtil from 'src/util/helper'
 
 interface IProps {
@@ -10,18 +12,24 @@ interface IProps {
 const TiledImageGallery: React.FunctionComponent<IProps> = ({
 	searchResults,
 }) => {
+	const dispatch = ReactRedux.useDispatch()
+
+	const openLightbox = (index: number) =>
+		dispatch(Actions.lightboxOpen(index))
+
 	return (
 		<div id="tiled-gallery">
-			{searchResults.map((result, id) => {
+			{searchResults.map((result, resultIndex) => {
 				return (
 					<img
 						title={result.uuid}
-						key={id}
+						key={resultIndex}
 						src={HelperUtil.thumbPath(
 							result.userId,
 							result.uuid,
 							's',
 						)}
+						onClick={() => openLightbox(resultIndex)}
 					/>
 				)
 			})}
