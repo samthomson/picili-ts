c32710e9dbfb48b11e7a465d305a462d8a3c22d7
Sam <samt@samt.st>
2022-01-22 09:43:11 +0000

    basic justified gallery working

--- app/spa/src/components/controls/SearchResults/JustifiedImageGallery.tsx
+++ app/spa/src/components/controls/SearchResults/JustifiedImageGallery.tsx
@@ -3,12 +3,18 @@ import useMeasure from 'react-use-measure'
 
 import * as Types from '@shared/declarations'
 import * as HelperUtil from 'src/util/helper'
+import { searchResults } from 'src/redux/selectors'
 
 interface IProps {
 	searchResults: Types.API.SearchResultItem[]
 }
 
-type Row = Types.API.SearchResultItem[]
+interface ScaledSearchResultItem extends Types.API.SearchResultItem {
+	scaledWidth: number
+	scaledHeight: number
+}
+
+type Row = ScaledSearchResultItem[]
 
 const JustifiedImageGallery = ({ searchResults }: IProps) => {
 	const [rows, setRows] = React.useState<Row[]>([])
@@ -26,33 +32,130 @@ const JustifiedImageGallery = ({ searchResults }: IProps) => {
 	const calculateJustifiedImageGallery = () => {
 		const holdingRows: Row[] = []
 		let rowInProgress: Row = []
+		const rowHeights: number[] = []
+
+		const baseRowHeight = 300
+		let currentRowHeight = baseRowHeight
+		const marginSize = 8
+		const scrollMargin = 24
+
+		let imagesInRow = 0
+		let runningWidth = 0
+
+		const availableWidth = width - scrollMargin
 
 		for (
 			let resultNo = 0, added = 1;
 			resultNo < searchResults.length;
 			resultNo++, added++
 		) {
+			// add image to current row under construction
 			const result = searchResults[resultNo]
-			rowInProgress.push(result)
-			if (added % 3 === 0) {
+			rowInProgress.push({
+				...result,
+				scaledWidth: result.mediumWidth,
+				scaledHeight: result.mediumHeight,
+			})
+			imagesInRow++
+
+			// get shortest in row
+			// take the first height as a base
+			let shortestImageInRowHeight = rowInProgress[0].mediumHeight
+			for (
+				let rowHeightCheck = 1;
+				rowHeightCheck < rowInProgress.length;
+				rowHeightCheck++
+			) {
+				if (
+					rowInProgress[rowHeightCheck].mediumHeight <
+					shortestImageInRowHeight
+				) {
+					shortestImageInRowHeight =
+						rowInProgress[rowHeightCheck].mediumHeight
+				}
+			}
+
+			// scale each to that height
+			runningWidth = 0
+			for (
+				let scaleEachInRow = 0;
+				scaleEachInRow < rowInProgress.length;
+				scaleEachInRow++
+			) {
+				const scaleFactor =
+					shortestImageInRowHeight /
+					rowInProgress[scaleEachInRow].mediumHeight
+				const scaledHeight =
+					rowInProgress[scaleEachInRow].mediumHeight * scaleFactor
+				const scaledWidth =
+					rowInProgress[scaleEachInRow].mediumWidth * scaleFactor
+
+				rowInProgress[scaleEachInRow].scaledHeight = scaledHeight
+				rowInProgress[scaleEachInRow].scaledWidth = scaledWidth
+
+				runningWidth += scaledWidth
+			}
+
+			// when over limit, calculate scaling factor, and add to structure of rows
+			const runningMarginWidth = marginSize * (imagesInRow - 1)
+			const runningWidthIncludingMargins =
+				runningWidth + runningMarginWidth
+
+			if (runningWidth > availableWidth - runningMarginWidth) {
+				const iOversizedRatio =
+					runningWidth / (availableWidth - runningMarginWidth)
+				const rowHeight = shortestImageInRowHeight / iOversizedRatio
+
+				for (
+					let iFinalScaleEachInRow = 0;
+					iFinalScaleEachInRow < rowInProgress.length;
+					iFinalScaleEachInRow++
+				) {
+					rowInProgress[iFinalScaleEachInRow].scaledHeight =
+						rowInProgress[iFinalScaleEachInRow].scaledHeight /
+						iOversizedRatio
+					rowInProgress[iFinalScaleEachInRow].scaledWidth =
+						rowInProgress[iFinalScaleEachInRow].scaledWidth /
+						iOversizedRatio
+				}
+				// add all to row and reset
 				holdingRows.push(rowInProgress)
+
 				rowInProgress = []
+				runningWidth = 0
+				imagesInRow = 0
+				currentRowHeight = baseRowHeight
+
+				rowHeights.push(rowHeight)
+			} else {
+				// put left over images into a row somehow? or squeeze into previous?
+				if (resultNo === searchResults.length - 1) {
+					// we're at the end
+					holdingRows.push(rowInProgress)
+					// to do, not 300 but it's actual height
+					rowHeights.push(rowInProgress[0].scaledHeight) // default
+				}
 			}
 		}
+
+		/*
+
 		if (rowInProgress.length > 0) {
 			holdingRows.push(rowInProgress)
 			rowInProgress = []
 		}
 		setRows(holdingRows)
+		*/
+		setRows(holdingRows)
 	}
 
 	return (
-		<div ref={ref}>
+		<div ref={ref} id="justified-gallery">
 			{width}
 			{/* display each row */}
 			{rows.map((row, rowIndex) => {
 				return (
-					<div key={rowIndex}>
+					<div key={rowIndex} className="justified-row">
 						{/* and every image in each row*/}
 						{row.map((result, id) => {
 							return (
@@ -64,6 +167,8 @@ const JustifiedImageGallery = ({ searchResults }: IProps) => {
 										result.uuid,
 										'm',
 									)}
+									width={`${result.scaledWidth}px`}
+									height={`${result.scaledHeight}px`}
 								/>
 							)
 						})}
