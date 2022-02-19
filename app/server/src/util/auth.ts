4b50911ff51c1312091161aaba01c268e128cfd2
Sam <samt@samt.st>
2022-02-19 17:05:09 +0000

    debug log server request inbound

--- app/server/src/util/auth.ts
+++ app/server/src/util/auth.ts
@@ -32,7 +32,8 @@ export const requestHasValidCookieToken = (ctx): boolean => {
 
 export const userIdFromRequestCookie = (req): string | undefined => {
     const authCookie = req?.cookies?.['picili-token']
-    Logger.info('cookies', req?.cookies)
+    Logger.info('req', req)
+    Logger.info('req?.cookies', req?.cookies)
 
     return userIdFromJWT(authCookie)
 }
