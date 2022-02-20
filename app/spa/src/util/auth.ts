c14b87044a69bdf2fe1082270ba0d183d6f373bb
Sam <samt@samt.st>
2022-02-20 15:45:27 +0000

    test cookie in prod

--- app/spa/src/util/auth.ts
+++ app/spa/src/util/auth.ts
@@ -1,6 +1,6 @@
 import Cookies from 'universal-cookie'
 
-const cookieName = 'picili-token'
+const cookieName = 'picili-spa'
 
 export const getToken = (): string | undefined => {
 	const cookies = new Cookies()
@@ -13,10 +13,23 @@ export const saveToken = (token: string): void => {
 
 	// set cookie
 	const cookies = new Cookies()
-	cookies.set(cookieName, token, { path: '/' })
+	cookies.set(cookieName, token, {
+		path: '/',
+		sameSite: 'none',
+		secure: true,
+		// domain: 'api.picili.dev',
+	})
 }
 
 export const removeToken = (): void => {
 	const cookies = new Cookies()
+	console.log('remove cookie', cookieName)
 	cookies.remove(cookieName)
+}
+
+export const getLocalAuthToken = (): string => {
+	// looks for an auth token cookie locally
+	const cookies = new Cookies()
+	const authCookieValue = cookies.get(cookieName)
+	return authCookieValue
 }
