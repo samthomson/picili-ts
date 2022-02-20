eb8bb8fbf76b917e2ae73f19adde2b00f912bc42
Sam <samt@samt.st>
2022-02-20 18:33:04 +0000

    derive api protocol via port

--- app/spa/src/util/helper.ts
+++ app/spa/src/util/helper.ts
@@ -1,6 +1,7 @@
 export const APIURL = (): string => {
 	const host = process.env.REACT_APP_API_HOST
 	const port = process.env.REACT_APP_API_PORT
-	const uri = `${window.location.protocol}//${host}:${port}/graphql`
+	const protocol = port === '443' ? 'https' : 'http'
+	const uri = `${protocol}://${host}:${port}/graphql`
 	return uri
 }
