707ab205572b573e8f068c183cb58c33a3f0d774
Sam <samt@samt.st>
2022-11-15 17:50:07 +0700

    explicitly install docker-compose on setup

--- initial-deployment.sh
+++ initial-deployment.sh
@@ -3,6 +3,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/.env
 
-docker-machine ssh $DOCKER_MACHINE_NAME "cd /; git clone https://github.com/samthomson/picili-ts.git"
+docker-machine ssh $DOCKER_MACHINE_NAME "cd /; git clone https://github.com/samthomson/picili-ts.git; apt install docker-compose -y"
 
-docker-machine scp .env $DOCKER_MACHINE_NAME:/picili-ts/.env
+docker-machine scp .env $DOCKER_MACHINE_NAME:/picili-ts/.
