70595b40204a8b061122a4840dff2d3794dbf468
sam <samt@samt.st>
2022-12-03 20:41:43 +0700

    admin page on mobile: make hamburger like menu

--- app/spa/src/components/pages/Admin/AdminTemplate.tsx
+++ app/spa/src/components/pages/Admin/AdminTemplate.tsx
@@ -1,11 +1,87 @@
 import * as React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
+import classNames from 'classnames'
 
 import PageTemplate from 'src/components/pages/PageTemplate'
 
 import useIsMobile from 'src/util/hooks/use-is-mobile.hook'
 interface Props {
 	children: React.ReactNode
+}
+
+const menuItems = [
+	{
+		href: '/admin',
+		displayText: 'Overview',
+	},
+	{
+		href: '/admin/dropbox',
+		displayText: 'Dropbox',
+	},
+	{
+		href: '/admin/tasks',
+		displayText: 'tasks',
+	},
+	{
+		href: '/admin/systemevents',
+		displayText: 'events',
+	},
+]
+
+const displayTextForRoute = (route: string): string => {
+	return menuItems.find((item) => item.href === route)?.displayText ?? 'Admin'
+}
+
+const MenuItems = () => {
+	return (
+		<ul>
+			{menuItems.map((menuItem, key) => (
+				<li key={key}>
+					<NavLink
+						exact={true}
+						className="item"
+						to={menuItem.href}
+						activeClassName="activeLink"
+					>
+						{menuItem.displayText}
+					</NavLink>
+				</li>
+			))}
+
+			{/*
+	<NavLink
+		exact={true}
+		className="item"
+		to="/admin/keys"
+	>
+		Keys
+	</NavLink> */}
+		</ul>
+	)
+}
+
+const DropdownMenu: React.FunctionComponent = () => {
+	const [isMenuOpen, setIsMenuOpen] = React.useState<boolean>(false)
+	const location = useLocation()
+	const isMobile = useIsMobile()
+
+	return (
+		<div
+			id="admin-dropdown-menu"
+			className={classNames({
+				'is-mobile': isMobile,
+			})}
+		>
+			<a onClick={() => setIsMenuOpen(!isMenuOpen)}>
+				<h2>{displayTextForRoute(location.pathname)}</h2>
+			</a>
+			{isMenuOpen && (
+				<div>
+					<MenuItems />
+				</div>
+			)}
+		</div>
+	)
 }
 
 const AdminTemplate: React.FunctionComponent<Props> = ({ children }: Props) => {
@@ -16,56 +92,13 @@ const AdminTemplate: React.FunctionComponent<Props> = ({ children }: Props) => {
 			<div id="admin-page">
 				{!isMobile && (
 					<div id="admin-side-menu">
-						<ul>
-							<li>
-								<NavLink
-									exact={true}
-									className="item"
-									to="/admin"
-								>
-									Overview
-								</NavLink>
-							</li>
-							<li>
-								<NavLink
-									exact={true}
-									className="item"
-									to="/admin/dropbox"
-								>
-									Dropbox
-								</NavLink>
-							</li>
-							<li>
-								<NavLink
-									exact={true}
-									className="item"
-									to="/admin/tasks"
-								>
-									tasks
-								</NavLink>
-							</li>
-							<li>
-								<NavLink
-									exact={true}
-									className="item"
-									to="/admin/systemevents"
-								>
-									events
-								</NavLink>
-							</li>
-
-							{/*
-							<NavLink
-								exact={true}
-								className="item"
-								to="/admin/keys"
-							>
-								Keys
-							</NavLink> */}
-						</ul>
+						<MenuItems />
 					</div>
 				)}
-				<div id="admin-tab-content">{children}</div>
+				<div id="admin-tab-content">
+					<DropdownMenu />
+					{children}
+				</div>
 			</div>
 		</PageTemplate>
 	)
