da47ef0dbb8fc46dcc220ae418e3af9f9bc22872
sam <samt@samt.st>
2022-12-03 15:02:48 +0700

    remove side menu

--- app/spa/src/components/pages/Search/SearchPageTemplate.tsx
+++ app/spa/src/components/pages/Search/SearchPageTemplate.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 
 import PageTemplate from 'src/components/pages/PageTemplate'
-import SideMenu from 'src/components/partials/SideMenu'
 import SearchInput from 'src/components/controls/SearchInput'
 import Lightbox from 'src/components/controls/Lightbox/index'
 
@@ -15,9 +14,6 @@ const SearchPageTemplate: React.FunctionComponent<Props> = ({
 	return (
 		<PageTemplate>
 			<Lightbox />
-			<div id="side-menu">
-				<SideMenu />
-			</div>
 			<div id="results-space">
 				<SearchInput />
 				{children}
