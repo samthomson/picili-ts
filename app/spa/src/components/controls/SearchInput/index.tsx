b430a5c9131551e918b41a1521255386824c9561
sam <samt@samt.st>
2022-12-23 10:54:03 +0700

    mantine in, search bar and mode toggle styling

--- app/spa/src/components/controls/SearchInput/index.tsx
+++ app/spa/src/components/controls/SearchInput/index.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react'
 import * as ReactRedux from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
+import {
+	Center,
+	Box,
+	SegmentedControl,
+	Paper,
+	UnstyledButton,
+} from '@mantine/core'
+import { IconLayout2, IconMap2, IconX } from '@tabler/icons'
 
 import * as Actions from 'src/redux/actions'
 import * as Selectors from 'src/redux/selectors'
@@ -20,46 +28,67 @@ const SearchInput: React.FunctionComponent = () => {
 
 	const resetQuery = () => dispatch(Actions.searchQueryReset())
 	const isMobile = useIsMobile()
+	const location = useLocation()
 
 	return (
 		<div id="search-bar">
 			<div id="search-input">
-				{individualQueries.map((individualQuery, index) => (
-					<IndividualQuery
-						key={index}
-						individualQuery={individualQuery}
-						disabled={isSearching}
-					/>
-				))}
-				<QueryInput disabled={isSearching} />
-				{isSearching && <>[searching icon/spinner]</>}
-				{individualQueries.length > 0 && (
-					<>
-						<button id="reset-query-button" onClick={resetQuery}>
-							[clear]
-						</button>
-					</>
-				)}
+				<Paper shadow="md">
+					<div id="quey-input-and-close-button">
+						<div id="queries-and-input">
+							{individualQueries.map((individualQuery, index) => (
+								<IndividualQuery
+									key={index}
+									individualQuery={individualQuery}
+									disabled={isSearching}
+								/>
+							))}
+							<QueryInput disabled={isSearching} />
+							{isSearching && <>[searching icon/spinner]</>}
+						</div>
+						<div id="clear-button-space">
+							{individualQueries.length > 0 && (
+								<UnstyledButton
+									id="reset-query-button"
+									onClick={resetQuery}
+								>
+									<IconX size={16} />
+								</UnstyledButton>
+							)}
+						</div>
+					</div>
+				</Paper>
 			</div>
 			{!isMobile && (
 				<div id="search-mode-toggle">
-					<NavLink
-						exact={true}
-						className="search-mode-toggle-option"
-						to="/"
-						activeClassName="activeLink"
-					>
-						[grid]
-					</NavLink>
-
-					<NavLink
-						exact={true}
-						className="search-mode-toggle-option"
-						to="/map"
-						activeClassName="activeLink"
-					>
-						[map]
-					</NavLink>
+					<SegmentedControl
+						size="md"
+						value={location.pathname === '/' ? 'grid' : 'map'}
+						data={[
+							{
+								value: 'grid',
+								label: (
+									<NavLink exact={true} to="/">
+										<Center>
+											<IconLayout2 size={16} />
+											<Box ml={10}>Grid</Box>
+										</Center>
+									</NavLink>
+								),
+							},
+							{
+								value: 'map',
+								label: (
+									<NavLink exact={true} to="/map">
+										<Center>
+											<IconMap2 size={16} />
+											<Box ml={10}>Map</Box>
+										</Center>
+									</NavLink>
+								),
+							},
+						]}
+					/>
 				</div>
 			)}
 		</div>
