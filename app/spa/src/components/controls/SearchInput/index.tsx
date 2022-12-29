fe203790d4fef7fd738bfb9d86a96d87998079ee
sam <samt@samt.st>
2022-12-29 13:29:16 +0700

    basic modal with tabs

--- app/spa/src/components/controls/SearchInput/index.tsx
+++ app/spa/src/components/controls/SearchInput/index.tsx
@@ -34,8 +34,56 @@ const SearchInput: React.FunctionComponent = () => {
 
 	// todo: populate search tabs from an array, use for modal too
 
+	const [searchMode, setSearchMode] = React.useState<undefined | string>(
+		undefined,
+	)
+
 	return (
 		<div id="search-tabs-and-search-bar">
+			<MantineCore.Modal
+				opened={typeof searchMode !== 'undefined'}
+				onClose={() => setSearchMode(undefined)}
+				title="Construct a search query"
+			>
+				searchMode: {searchMode}
+				<MantineCore.Tabs variant="outline" value={searchMode}>
+					<MantineCore.Tabs.List>
+						<MantineCore.Tabs.Tab
+							value="text"
+							onClick={() => setSearchMode(undefined)}
+							// icon={<IconPhoto size={14} />}
+						>
+							Text search
+						</MantineCore.Tabs.Tab>
+						<MantineCore.Tabs.Tab
+							value="elevation"
+							onClick={() => setSearchMode('elevation')}
+							// icon={<IconMessageCircle size={14} />}
+						>
+							Elevation
+						</MantineCore.Tabs.Tab>
+						<MantineCore.Tabs.Tab
+							value="colour"
+							onClick={() => setSearchMode('colour')}
+							// icon={<IconSettings size={14} />}
+						>
+							Colour
+						</MantineCore.Tabs.Tab>
+					</MantineCore.Tabs.List>
+
+					<MantineCore.Tabs.Panel value="gallery" pt="xs">
+						Gallery tab content
+					</MantineCore.Tabs.Panel>
+
+					<MantineCore.Tabs.Panel value="messages" pt="xs">
+						Messages tab content
+					</MantineCore.Tabs.Panel>
+
+					<MantineCore.Tabs.Panel value="settings" pt="xs">
+						Settings tab content
+					</MantineCore.Tabs.Panel>
+				</MantineCore.Tabs>
+			</MantineCore.Modal>
 			<MantineCore.Tabs variant="outline" radius="md" value="text">
 				<MantineCore.Tabs.List>
 					<MantineCore.Tabs.Tab
@@ -47,15 +95,14 @@ const SearchInput: React.FunctionComponent = () => {
 					<MantineCore.Tabs.Tab
 						value="elevation"
 						// icon={<IconMessageCircle size={14} />}
-						onClick={() => {
-							console.log('clicked elevation')
-						}}
+						onClick={() => setSearchMode('elevation')}
 					>
 						Elevation
 					</MantineCore.Tabs.Tab>
 					<MantineCore.Tabs.Tab
 						value="colour"
 						// icon={<IconSettings size={14} />}
+						onClick={() => setSearchMode('colour')}
 					>
 						Colour
 					</MantineCore.Tabs.Tab>
