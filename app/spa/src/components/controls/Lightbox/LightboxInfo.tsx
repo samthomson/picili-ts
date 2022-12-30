67a50ce191849a00cd77af46129c28ecf2c4a615
sam <samt@samt.st>
2022-12-30 12:06:18 +0700

    lightbox styling

--- app/spa/src/components/controls/Lightbox/LightboxInfo.tsx
+++ app/spa/src/components/controls/Lightbox/LightboxInfo.tsx
@@ -34,6 +34,106 @@ interface IProps {
 	fileId: number
 }
 
+// todo: memoize
+const IconForType = (type: string, subtype: string) => {
+	switch (true) {
+		case type === 'location':
+			return Icons.IconMapPin
+
+		case type === 'exif':
+		case type === 'metadata':
+			return Icons.IconAperture
+
+		case type === 'folder':
+			return Icons.IconFolder
+
+		case type === 'subject':
+			return Icons.IconPhotoSearch
+
+		case type === 'ocr' && subtype === 'text':
+			return Icons.IconTextRecognition
+
+		case type === 'ocr' && subtype === 'number_plate':
+			return Icons.IconCarCrash
+
+		case type === 'fileextension':
+		case type === 'filetype':
+		case type === 'filename':
+			return Icons.IconFileInfo
+
+		// case type === Enums.QueryType.ELEVATION:
+		// 	return Icons.IconMountain
+
+		// todo: plant IconPlant
+		// todo: directory
+
+		default:
+			return Icons.IconQuestionMark
+	}
+}
+
+// https://stackoverflow.com/a/18650828/686490
+const formatBytes = (bytes: number, decimals = 2): string => {
+	if (!+bytes) return '0 Bytes'
+
+	const k = 1024
+	const dm = decimals < 0 ? 0 : decimals
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+	const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+	return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
+const displayContentForTag = (tag: Types.API.Tag) => {
+	const { type, subtype, value, confidence } = tag
+
+	switch (true) {
+		case type === 'fileextension':
+			return `extension: ${value}`
+		case type === 'filename':
+			return `filename: ${value}`
+		case type === 'filetype':
+			return `type: ${value}`
+		case type === 'folder':
+			return `folder: ${value}`
+		case type === 'location':
+			return `${subtype}: ${value}`
+
+		case type === 'exif' && subtype === 'exposuretime':
+			const fraction = 1 / +value
+			return `exposure: 1/${fraction}`
+
+		case type === 'exif' && subtype === 'orientation':
+			return `orientation: ${+value === 1 ? 'landscape' : 'portrait'}`
+
+		case type === 'exif' && subtype === 'focallength':
+			return `${value}mm`
+
+		case type === 'metadata' && subtype === 'length':
+			return (
+				'length: ' +
+				moment
+					.utc(
+						moment.duration({ seconds: +value }).as('milliseconds'),
+					)
+					.format('HH:mm:ss')
+			)
+
+		case type === 'metadata' && subtype === 'size':
+			return formatBytes(+value)
+
+		case type === 'exif':
+		case type === 'metadata':
+			return `${subtype}: ${value}`
+
+		case type === 'subject' && subtype === 'imagga':
+			return `${value} (${confidence}%)`
+		default:
+			return `${type}.${subtype}=${value}(${confidence}%)`
+	}
+}
+
 const LightboxInfo: React.FunctionComponent<IProps> = ({
 	isShowing,
 	fileId,
@@ -61,12 +161,26 @@ const LightboxInfo: React.FunctionComponent<IProps> = ({
 
 	const { fileInfo }: { fileInfo: Types.API.FileInfo } = data
 
-	const searchFromTag = (tagValue: string) => {
-		dispatch(Actions.searchQueryAdd({ value: tagValue }))
+	const searchFromTag = (type: string, subtype: string, tagValue: string) => {
+		dispatch(Actions.searchQueryAdd({ type, subtype, value: tagValue }))
 		dispatch(Actions.attemptSearch())
 		// close the lightbox
 		dispatch(Actions.lightboxClose())
 	}
+
+	// todo: memoize
+	const tagGroups: Record<string, Types.API.Tag[]> = (() => {
+		const groups: Record<string, Types.API.Tag[]> = {}
+		for (let i = 0; i < fileInfo?.tags.length; i++) {
+			const tag = fileInfo.tags[i]
+
+			if (!groups?.[tag.type]) {
+				groups[tag.type] = []
+			}
+			groups[tag.type].push(tag)
+		}
+		return groups
+	})()
 	return (
 		<>
 			{fileInfo?.address && (
@@ -97,33 +211,36 @@ const LightboxInfo: React.FunctionComponent<IProps> = ({
 				</div>
 			)}
 
-			<p>
-				{fileInfo?.pathOnDropbox && (
-					<>on dropbox: {fileInfo.pathOnDropbox} </>
-				)}
-			</p>
-			{fileInfo?.tags && (
-				<table>
-					<tbody>
-						{fileInfo.tags.map((tag, tagIndex) => (
-							<tr key={tagIndex}>
-								<td>
-									{tag.type}
-									{tag?.subtype ? `: ${tag.subtype}` : ''}
-								</td>
-								<td>
-									{/* todo: later ensure this has a cursor:
-									pointer */}
-									<a onClick={() => searchFromTag(tag.value)}>
-										{tag.value}
-									</a>
-								</td>
-								<td>{tag.confidence}%</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
+			{fileInfo?.pathOnDropbox && (
+				<div className="lightbox-information-piece">
+					<Icons.IconFolders size={14} />
+					{fileInfo.pathOnDropbox}
+				</div>
 			)}
+
+			{Object.values(tagGroups).map((group, groupIndex) => {
+				return (
+					<div key={groupIndex} className="lightbox-tag-group">
+						{group.map((tag, tagIndex) => {
+							const { type, subtype, value } = tag
+
+							const Icon = IconForType(type, subtype)
+							return (
+								<div
+									key={tagIndex}
+									className="lightbox-tag"
+									onClick={() =>
+										searchFromTag(type, subtype, value)
+									}
+								>
+									<Icon size={14} />{' '}
+									<span>{displayContentForTag(tag)}</span>
+								</div>
+							)
+						})}
+					</div>
+				)
+			})}
 		</>
 	)
 }
