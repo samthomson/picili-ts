4ff3d70b9a367adfd871b2545965c8bf9a730905
sam <samt@samt.st>
2023-01-07 14:11:54 +0700

    keep scroll position after closing lightbox

--- app/spa/src/components/pages/Search/SearchPageTemplate.tsx
+++ app/spa/src/components/pages/Search/SearchPageTemplate.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
-import * as ReactRedux from 'react-redux'
 
 import PageTemplate from 'src/components/pages/PageTemplate'
 import SearchInput from 'src/components/controls/SearchInput'
 import Lightbox from 'src/components/controls/Lightbox/index'
-import * as Selectors from 'src/redux/selectors'
 
 interface Props {
 	children: React.ReactNode
@@ -13,18 +11,11 @@ interface Props {
 const SearchPageTemplate: React.FunctionComponent<Props> = ({
 	children,
 }: Props) => {
-	const lightboxIndex = ReactRedux.useSelector(Selectors.lightboxIndex)
-
 	return (
 		<PageTemplate>
 			<Lightbox />
 			{/* disable scrolling on results when the lightbox is showing to remove scrollbars there. */}
-			<div
-				id="results-space"
-				style={{
-					overflow: typeof lightboxIndex === 'number' ? 'hidden' : '',
-				}}
-			>
+			<div id="results-space">
 				<SearchInput />
 				{children}
 			</div>
