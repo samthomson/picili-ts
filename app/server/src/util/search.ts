60371af09ea07bd64035dd92cb813e9b139ad321
sam <samt@samt.st>
2023-01-20 19:45:43 +0700

    fix: not always showing geoaggregations on map

--- app/server/src/util/search.ts
+++ app/server/src/util/search.ts
@@ -189,7 +189,7 @@ export const autoComplete = async (
 }
 
 export const geoAggregateResults = (
-    results: Types.Core.DBSearchMatch[],
+    results: Types.API.SearchResultItem[],
     mapBounds: number[], // [-180, -85, 180, 85]
     zoom: number, // 2
     userId: number,
